8830c94ca17a5351cf5b742fd4ab2dd1a29970e7
Yannis1112 <99808536+Yannis1112@users.noreply.github.com>
2025-04-05 12:32:53 +0000

    Deploying to gh-pages from @ Yannis1112/yannis1112.github.io@f7dc47c88d5f3451971275be3c662faf93661aaf 🚀

---
+++ web_plugin_registrant.dart
@@ -0,0 +1,6 @@
+import 'package:portfolio/utils/web_asset_handler.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins(Registrar registrar) {
+  AssetHandler.registerWith(registrar);
+}
